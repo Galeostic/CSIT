7bc541da17290e455824a9c7ca05900fd6478a9d
Galeostic <Galeostic@users.noreply.github.com>
2017-05-29 14:17:34 +0700

    Update fac_cl.php
    เพิ่ม function insert/update/delete

--- application/controllers/fac_cl.php
+++ application/controllers/fac_cl.php
@@ -27,9 +27,46 @@ class Fac_Cl extends CI_Controller {
       
       
     }
+    
     function get_fac_edit(){
      $var['rs']=$this->Fac_Model->get_fac(); 
         $this->load->view('admin/Show',$var);
+    }
+    
+    function insert_fac()//เพิ่มข้อมูล
+    {
+         $data = array(
+            'fac_id' => $this->input->post('fac_id'),
+            'fac_name' => $this->input->post('fac_name'),
+            'create_date' => date('Y-m-d H:i:s')
+        );
+         if ($this->Admin_Model->add_fac($data)) {
+            $this->load->view('admin/add_fac', $data);
+        }
+    }
+    
+     function update_fac()//update major
+    {
+        $data=array(
+                 'fac_id' => $this->input->post('fac_id'),
+                'fac_name' => $this->input->post('fac_name'),
+                'create' => date('Y-m-d H:i:s')
+                );
+        $this->db->where('id', $this->input->post('fac_id'));
+        if($this->db->update('tb_facuty', $data))
+            return TRUE;
+        else
+            return FALSE;
+    }    
+    
+    
+    function delete_fac() // delete
+    {
+        if($this->major_model->del_fac()== TRUE) { //เรียกใช้ del_fac ใน fac_model
+            $data['result']=" ลบข้อมูลเรียบร้อยแล้ว ";
+        } else {
+            $data['result']=" ไม่สามารถลบข้อมูลได้";
+        }
     }
 }
  
