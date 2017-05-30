107819d72c75c9dcf654fe6017c4dbc88a3e5444
Galeostic <Galeostic@users.noreply.github.com>
2017-05-30 10:07:50 +0700

    เพิ่ม function insert
    เพิ่ม function insert  fac
    เพิ่ม function insert  major

--- application/controllers/admin.php
+++ application/controllers/admin.php
@@ -2,28 +2,43 @@
 
 class Admin extends CI_Controller {
 
-	function __construct() {
+       public function __construct() {
         parent::__construct();
-        $this->load->model('Admin_Model');  //ทำการload model admin model เข้ามาใช้งาน
+       // $this->load->model('Admin_Model');  //ทำการload model admin model เข้ามาใช้งาน
         $this->load->helper('url');//เรียกใช้ helper url
 	/**ส่วนของcontroller  login ทำงานเป็นส่วนแรกของระบบในการตรวจสอบการทำงาน
 	function indexเรียกหน้า login ใน front/login */
-
-	public function index()
-	{
-		$this->load->view('admin/admin_page');
-	
-}
+       }
+     
 // function insert facutly 
-    function  add_fac(){
-        
-    }
+     public function  add_fac(){
+       $data=array(
+                'fac_id' => $this->input->post('fac_id'),
+                'fac_name' => $this->input->post('fac_name')
+                );
+        $query=$this->db->insert('tb_facuty', $data);
+        if($query) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+        }
     
-    function  add_major(){
-        //function insert major 
-    }
+         
     
-
-
+    public function  add_major(){
+   $data=array(
+                'major_id' => $this->input->post('major_id'),
+                'major_name' => $this->input->post('major_name')
+                );
+        $query=$this->db->insert('tb_major', $data);
+        if($query) {
+            return TRUE;
+        } else {
+            return FALSE;
+        }
+    }
 }
+        
+
 
